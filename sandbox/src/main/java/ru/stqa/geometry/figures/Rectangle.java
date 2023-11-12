562995f42a07bdfaf61bffc86d065de6b1517958
Kristina Kravtsova <grezdingo@ya.ru>
2023-11-13 02:43:36 +0300

    Added exceptions to square and rectangle

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -2,6 +2,12 @@ package ru.stqa.geometry.figures;
 
 public record Rectangle(double a, double b) {
 
+    public Rectangle {
+        if (a < 0 || b < 0) {
+            throw new IllegalArgumentException("Rectangle side should be non-negative");
+        }
+    }
+
     public static void printArea(Rectangle rectangle) {
         String text = String.format("The area of the rectangle with sides %f and %f = %f",
                 rectangle.a, rectangle.b, rectangle.area());
--- sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Square.java
@@ -2,6 +2,12 @@ package ru.stqa.geometry.figures;
 
 public record Square(double side) {
 
+    public Square {
+        if (side < 0) {
+            throw new IllegalArgumentException("Square side should be non-negative");
+        }
+    }
+
     public static void printArea(Square square) {
         String text = String.format("The area of the square with side %f = %f",
                 square.side, square.area());
