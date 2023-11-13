34a66c8320f5405d2d7ac6f6415680b8c887452a
Kristina Kravtsova <grezdingo@ya.ru>
2023-11-13 16:54:15 +0300

    Added new square and rectangle tests, modified equals method

--- sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
+++ sandbox/src/main/java/ru/stqa/geometry/figures/Rectangle.java
@@ -1,6 +1,22 @@
 package ru.stqa.geometry.figures;
 
+import java.util.Objects;
+
 public record Rectangle(double a, double b) {
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Rectangle rectangle = (Rectangle) o;
+        return (Double.compare(this.a, rectangle.a) == 0 && Double.compare(this.b, rectangle.b) == 0)
+                || (Double.compare(this.a, rectangle.b) == 0 && Double.compare(this.b, rectangle.a) == 0);
+    }
+
+    @Override
+    public int hashCode() {
+        return 1;
+    }
 
     public Rectangle {
         if (a < 0 || b < 0) {
